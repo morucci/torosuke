2993f9a917df4f75f2ddbcd7a9f5d1f62d4e56ca
Fabien Boucher <fabien.dot.boucher@gmail.com>
2021-08-15 15:30:47 +0000

    Pass until date to Fetcher

--- src/Torosuke/Runner.hs
+++ src/Torosuke/Runner.hs
@@ -1,27 +1,22 @@
 module Torosuke.Runner where
 
 import qualified Data.HashMap.Strict as HM
+import Data.Time.Clock
 import Relude
 import Torosuke.Binance
 import Torosuke.Store
 import Torosuke.Ta
 import Torosuke.Types
 
-pairFetcherAndAnalyzer :: Pair -> Interval -> IO ()
-pairFetcherAndAnalyzer pair interval = do
+pairFetcherAndAnalyzer :: Pair -> Interval -> Maybe UTCTime -> IO ()
+pairFetcherAndAnalyzer pair interval until = do
   let klinesDP = getKlinesDumpPath pair interval
       klinesDPLast100 = getKlinesDumpPathLast100 pair interval
       klinesAnalysis = getKlinesAnalysisDumpPath pair interval
   stored <- loadKlines klinesDP
-  resp <- getKlines pair interval 500 Nothing
+  resp <- getKlines pair interval 500 until
   let (KlinesHTTPResponse status _ fetchedM) = resp
-  print $
-    "Fetching - "
-      <> pairToText pair
-      <> " at interval:"
-      <> intervalToText interval
-      <> " - status:"
-      <> show status
+  log pair interval status
   let (updatedKlines, analysis) = case (stored, fetchedM) of
         (Nothing, Nothing) -> error "Unable to decode dump and to fetch from API"
         (Just _, Nothing) -> error "Unable to fetch from API"
@@ -35,9 +30,22 @@ pairFetcherAndAnalyzer pair interval = do
       Klines $
         HM.elems $
           HM.union (unKlinesHM $ toKlinesHM set1) (unKlinesHM $ toKlinesHM set2)
+    log pair' interval' status =
+      print $
+        "Fetching 500 candles - pair: "
+          <> pairToText pair'
+          <> ", interval: "
+          <> intervalToText interval'
+          <> ", until: "
+          <> ( if isJust until
+                 then show until
+                 else "now"
+             )
+          <> " - status: "
+          <> show status
 
 runner :: IO ()
 runner = do
-  _ <- pairFetcherAndAnalyzer ADAUSDT ONE_D
-  _ <- pairFetcherAndAnalyzer ADAUSDT ONE_H
+  _ <- pairFetcherAndAnalyzer ADAUSDT ONE_D Nothing
+  _ <- pairFetcherAndAnalyzer ADAUSDT ONE_H Nothing
   pure ()
