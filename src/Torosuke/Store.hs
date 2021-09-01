cd03c307ff52e449ceb453767e8b5b2436b1f7e8
Fabien Boucher <fabien.dot.boucher@gmail.com>
2021-09-01 14:31:20 +0000

    store - dump only if content change

--- src/Torosuke/Store.hs
+++ src/Torosuke/Store.hs
@@ -1,20 +1,30 @@
 module Torosuke.Store where
 
+import Control.Exception.Safe
 import Data.Aeson (ToJSON, decode, encode)
 import Relude
 import System.Directory (createDirectoryIfMissing, doesFileExist, renameFile)
 import Torosuke.Types
 
 dumpData :: ToJSON a => DumpPath -> a -> ReaderT Env IO ()
-dumpData dpath kls = do
+dumpData dpath jdata = do
   env <- ask
   let cacheDir = ".cache" <> "/" <> dpDir dpath
   let cachePath = cacheDir <> "/" <> dpName dpath
   liftIO $ createDirectoryIfMissing True cacheDir
-  let content = decodeUtf8 $ encode kls
-  writeFile cachePath content
-  liftIO $ renameFile cachePath $ show dpath
-  liftIO $ envLog env $ "Wrote " <> show dpath
+  let content = decodeUtf8 $ encode jdata
+  current_content <- liftIO $ getDataFromFile $ show dpath
+  when (current_content /= content) $ do
+    writeFile cachePath content
+    liftIO $ renameFile cachePath $ show dpath
+    liftIO $ envLog env $ "Wrote " <> show dpath
+
+getDataFromFile :: String -> IO String
+getDataFromFile path = do
+  result <- tryAny $ readFile path
+  case result of
+    Right content -> pure content
+    Left _ -> pure ""
 
 loadKlines :: DumpPath -> IO (Maybe Klines)
 loadKlines dpath = do
