fe43cc1e3442f75bea132a4589088304c650ced9
Fabien Boucher <fabien.dot.boucher@gmail.com>
2021-11-10 15:45:03 +0000

    Add scrollable viewport

--- src/Torosuke/Ui.hs
+++ src/Torosuke/Ui.hs
@@ -6,17 +6,16 @@ import Control.Concurrent (forkIO, threadDelay)
 import qualified Graphics.Vty as V
 import Relude
 
-ui :: Text -> Widget ()
-ui = str . toString
-
 data Tick = Tick
+
+data Name = Viewport1 deriving (Eq, Ord, Show)
 
 data AppState = AppState
   { items :: [Text],
     ticks :: Int
   }
 
-app :: App AppState Tick ()
+app :: App AppState Tick Name
 app =
   App
     { appDraw = drawUI,
@@ -26,11 +25,19 @@ app =
       appAttrMap = const theMap
     }
 
-drawUI :: AppState -> [Widget ()]
-drawUI s = [hBox $ ui <$> (items s <> [show $ s & ticks])]
+drawUI :: AppState -> [Widget Name]
+drawUI s = [viewport Viewport1 Vertical $ vBox $ str . toString <$> (items s <> [show $ s & ticks])]
 
-handleEvent :: AppState -> BrickEvent () Tick -> EventM () (Next AppState)
+handleEvent :: AppState -> BrickEvent Name Tick -> EventM Name (Next AppState)
 handleEvent s (VtyEvent (V.EvKey V.KEsc [])) = halt s
+handleEvent s (VtyEvent (V.EvKey V.KDown [])) = do
+  let vp = viewportScroll Viewport1
+  vScrollBy vp 1
+  continue s
+handleEvent s (VtyEvent (V.EvKey V.KUp [])) = do
+  let vp = viewportScroll Viewport1
+  vScrollBy vp (-1)
+  continue s
 handleEvent s (AppEvent Tick) = do
   let nS = s {ticks = (s & ticks) + 1}
   continue nS
@@ -41,7 +48,7 @@ theMap = attrMap V.defAttr []
 
 main :: IO ()
 main = do
-  let initialState = AppState ["Hello", "Word", "!"] 0
+  let initialState = AppState (["Hello", "Word", "!"] <> (replicate 20 "a")) 0
       buildVty = V.mkVty V.defaultConfig
   chan <- newBChan 10
   initialVty <- buildVty
