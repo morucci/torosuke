6dc0ae733dc75e2bdab9054118e8870c32d42f42
Fabien Boucher <fabien.dot.boucher@gmail.com>
2021-11-10 07:51:28 +0000

    [tui] added external app tick event

--- src/Torosuke/Ui.hs
+++ src/Torosuke/Ui.hs
@@ -1,18 +1,22 @@
 module Torosuke.Ui where
 
 import Brick
+import Brick.BChan
+import Control.Concurrent (forkIO, threadDelay)
 import qualified Graphics.Vty as V
 import Relude
 
 ui :: Text -> Widget ()
 ui = str . toString
 
+data Tick = Tick
+
 data AppState = AppState
   { items :: [Text],
-    otherItems :: [Text]
+    ticks :: Int
   }
 
-app :: App AppState e ()
+app :: App AppState Tick ()
 app =
   App
     { appDraw = drawUI,
@@ -23,10 +27,13 @@ app =
     }
 
 drawUI :: AppState -> [Widget ()]
-drawUI s = [hBox $ ui <$> items s]
+drawUI s = [hBox $ ui <$> (items s <> [show $ s & ticks])]
 
-handleEvent :: AppState -> BrickEvent () e -> EventM () (Next AppState)
+handleEvent :: AppState -> BrickEvent () Tick -> EventM () (Next AppState)
 handleEvent s (VtyEvent (V.EvKey V.KEsc [])) = halt s
+handleEvent s (AppEvent Tick) = do
+  let nS = s {ticks = (s & ticks) + 1}
+  continue nS
 handleEvent s _ = continue s
 
 theMap :: AttrMap
@@ -34,5 +41,14 @@ theMap = attrMap V.defAttr []
 
 main :: IO ()
 main = do
-  let initialState = AppState ["Hello", "Word", "!"] []
-  void $ defaultMain app initialState
+  let initialState = AppState ["Hello", "Word", "!"] 0
+      buildVty = V.mkVty V.defaultConfig
+  chan <- newBChan 10
+  initialVty <- buildVty
+  void
+    . forkIO
+    . forever
+    $ do
+      writeBChan chan Tick
+      threadDelay 100000
+  void $ customMain initialVty buildVty (Just chan) app initialState
